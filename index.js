457fd7629383e594f9ad8457a0f68a9adab4f660
dansan-dsn <ddryn970@gmail.com>
2024-04-28 16:12:38 +0300

    req-events

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+    res.send('Hello, World')
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3])
+});
+
+app.get('/api/posts/:year/:month',(req, res) => {
+    res.send(req.params)
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`)
+})
